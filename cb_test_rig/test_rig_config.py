a900705e6de929e0efd3a8d3e6385c7ff45807ab
Thomas Popp <Thomas@chriesibaum.com>
2025-08-20 14:57:16 +0200

    codestyle++

--- cb_test_rig/test_rig_config.py
+++ cb_test_rig/test_rig_config.py
@@ -2,6 +2,7 @@ import configparser
 
 CONFIG_FILE_NAME = 'test_rig.conf'
 
+
 class TestRigConfig():
     def __init__(self, mqtt_broker='localhost', mqtt_broker_port=1883,
                  test_name=''):
@@ -23,16 +24,16 @@ class TestRigConfig():
 
         config.read(conf_file_name)
 
-        self.mqtt_broker = config.get('MQTT-BROKER', 'host', fallback=self.mqtt_broker)
-        self.mqtt_broker_port = config.getint('MQTT-BROKER', 'port', fallback=self.mqtt_broker_port)
-
-
+        self.mqtt_broker = config.get(
+            'MQTT-BROKER', 'host', fallback=self.mqtt_broker)
+        self.mqtt_broker_port = config.getint(
+            'MQTT-BROKER', 'port', fallback=self.mqtt_broker_port)
 
     def write_config(self, conf_file_name=CONFIG_FILE_NAME):
         config = configparser.ConfigParser()
 
         config['MQTT-BROKER'] = {'host': self.mqtt_broker,
-                                  'port': str(self.mqtt_broker_port)}
+                                 'port': str(self.mqtt_broker_port)}
 
         config['TEST'] = {'name': self.test_name}
 
